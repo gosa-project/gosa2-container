9c45f2bf9e1135ee06b415f8fc8ff47be8ee83a6
Benjamin Zapiec <zapiec@gonicus.de>
2024-07-03 09:32:57 +0200

    add initial entrypoint

---
+++ container/ldap/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+command="$(command -v slapd)"
+
+echo $LDAP_TYPE
+
+find /etc/ldap/
+if [ ! -f /etc/ldap/slapd.d/cn=config.ldif ]; then
+	echo "Applying initial configuration"
+	slapadd -l /etc/opt/init/config.ldif -b cn=config -F /etc/ldap/slapd.d
+	slapadd -l /etc/opt/init/datanew6.ldif -F /etc/ldap/slapd.d
+fi
+
+# if [ ! -f /var/lib/ldap/data.mdb ]; then
+# 	xz --decompress --to-stdout /etc/opt/init/init.ldif.xz | slapadd -q -b dc=example,dc=de
+# fi
+
+exec /usr/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME" -d 256
